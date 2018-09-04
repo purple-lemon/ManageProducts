cb505697c3f515eedc48a8a036a34d606beae731
byurch <byurchak@gmail.com>
2018-09-04 13:40:46 +0300

    Add base unit tests

---
+++ App/ProductManager/ProductManager.UnitTests/ExportTests.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using ProductManager.Contracts.Services;
+using ProductManager.Models.DTO;
+using ProductManager.Services;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ProductManager.XUnit
+{
+	public class ExportTests
+	{
+		[Fact]
+		public void ExportTest()
+		{
+			var productsStub = new List<FullProductDTO>()
+			{
+				new FullProductDTO(){ Id = 1, Code = "123", Name = "Cheese", Price = 100m },
+				new FullProductDTO(){ Id = 24, Code = "321", Name = "Bread", Price = 23m },
+			};
+			var productServiceMoq = new Mock<IProductService>();
+			productServiceMoq.Setup(x => x.Get()).Returns(productsStub);
+			var exportService = new DataExporter(productServiceMoq.Object);
+			var package = exportService.GetPackage();
+			var ws = package.Workbook.Worksheets["Products"];
+			Assert.NotNull(ws);
+
+			foreach (var p in productsStub)
+			{
+				var row = productsStub.IndexOf(p) + 2;
+				Assert.Equal(ws.Cells[row, 1].GetValue<string>(), p.Id.ToString());
+				Assert.Equal(ws.Cells[row, 2].GetValue<string>(), p.Code.ToString());
+				Assert.Equal(ws.Cells[row, 3].GetValue<string>(), p.Name.ToString());
+				Assert.Equal(ws.Cells[row, 4].GetValue<string>(), p.Price.ToString());
+			}
+			package.Dispose();
+		}
+	}
+}
